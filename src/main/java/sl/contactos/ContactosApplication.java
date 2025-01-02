79f4dc75d9f9c5ac79317a4925bb34389a3d66cd
Samuel Lopez <samuellopez01@hotmail.com>
2025-01-01 22:15:17 -0500

    Creación del proyecto con Spring y Thymeleaf

---
+++ src/main/java/sl/contactos/ContactosApplication.java
@@ -0,0 +1,13 @@
+package sl.contactos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactosApplication.class, args);
+    }
+
+}
