7a69c8711fecdf3111fca209717266e5be2757d3
Samuel Lopez <samuellopez01@hotmail.com>
2025-01-03 09:52:24 -0500

    Listado de contactos con Thymeleaf y Spring

--- src/main/java/sl/contactos/modelo/Contacto.java
+++ src/main/java/sl/contactos/modelo/Contacto.java
@@ -21,6 +21,38 @@ public class Contacto {
     String celular;
     String email;
 
+    public Integer getIdContacto() {
+        return idContacto;
+    }
+
+    public void setIdContacto(Integer idContacto) {
+        this.idContacto = idContacto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     @Override
     public String toString() {
         return "Contacto{" +
